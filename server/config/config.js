f649e72c0f7e64d5c2f7daecf44fcc98e2c1ff7c
Kamil Chomiak <kchomiak@outlook.com>
2025-05-10 01:25:36 -0400

    set up user model

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,3 @@
-const path = require("path");
-const envPath = path.resolve(__dirname, "../.env");
-require("dotenv").config({ path: envPath });
-
-console.log("ENV:", {
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
-  db: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-});
-
 module.exports = {
   development: {
     username: process.env.DB_USER,
@@ -18,11 +6,5 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-  },
-  test: {
-    // similar if needed
-  },
-  production: {
-    // use production env values
   },
 };
